1b69f9adb9045fc652c8d182e815967280e89e4a
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-12-09 23:04:47 -0600

    Improved distribution detection function
    Also added a kernel version function (unstable)

--- sestoolkit.sh
+++ sestoolkit.sh
@@ -3,7 +3,7 @@
 # Arguments handler
 for arg in "$@"
 do
-    if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]
+    if [ "$arg" = "--help" ] || [ "$arg" = "-h" ]
     then
         echo -e "\e[32mSecurity Strike Toolkit Help\e[39m"
         echo -e "Usage: "
@@ -13,13 +13,13 @@ done
 # Architecture Detect
 arch=$(uname -m)
 
-if [[ arch != "x86_64" ]];
+if [[ $arch != "x86_64" ]];
 then
     echo -e "Your architecture is not AMD64, some functions may not work properly"
 fi
 
 # OS detection
-function detect_os {
+detect_os() {
     if [[ "$OSTYPE" == "linux-gnu" ]];
     then
         detect_distro
@@ -39,11 +39,26 @@ function detect_os {
     then
         echo -e "\e[33mYou are using a BSD machine, script may or may not work. We haven't done testing\e[39m"
     else
-        exit 2;
+        exit 2
     fi
 }
 
 # Linux Distribution detection
-function detect_distro {
+detect_distro() {
+    if [  -n "$(gcc -version | grep Ubuntu)" ]; then
+        echo -e "You are running Ubuntu"
+    elif [ -n "$(gcc -version | grep Debian)" ]; then
+        echo -e "You are running Debian"
+    elif [ -n "$(gcc -version | grep Manjaro)" ]; then
+        echo -e "You are running Manjaro"
+    fi
+}
 
+# Find Kernel Version
+kernel_version() {
+    kernel=$(awk -F. '{print $1$2}' <<< $(uname -r))
+
+    if [ $kernel -lt 30 ]; then
+        echo -e "This script doesn't support any kernel version prior to 3.0.0"
+    fi
 }
