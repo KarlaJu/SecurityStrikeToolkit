a1fb00e36f509981d628272f6ab451baa213118d
VentGrey <omarpurataf@gmail.com>
2020-04-16 19:34:42 -0500

    Added lib-era with a small example

---
+++ lib/era.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Era Library for common functions used across
+# modules.
+
+check_dep() {
+	command -v "$1" > /dev/null 2>&1
+	if [ "$?" ]; then
+	echo "[✔]::[$1]: installation found!";
+	else
+
+	echo "[x]::[warning]:this module requires $1" ;
+	echo ""
+	echosleep 2
+	echo ""
+fi
+	sleep 1
+}
+
+# 2020 - SecStrikeToolkit
