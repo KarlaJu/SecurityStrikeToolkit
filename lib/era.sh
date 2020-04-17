a6b5e12e9bcd0a3987447bb90fca024915c38783
VentGrey <omarpurataf@gmail.com>
2020-04-16 22:36:57 -0500

    Nevermind

--- lib/era.sh
+++ lib/era.sh
@@ -22,11 +22,4 @@ check_root() {
 		echo "This module must be run as root"; exit 1
 }
 
-is_multilib() {
-	if $(uname -m ) = "x86_64" ; then
-	echo "Multilib is not supported!"; exit 2
-	else
-	echo "[-] multilib is already enabled!"
-	fi
-}
 # 2020 - SecStrikeToolkit
